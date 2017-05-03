baeeedee69e14f61568e92c7a7ada1cf6ca7e246
wei cheng <michael1996623@gmail.com>
2017-05-04 01:27:43 +0800

    tutorial ant project

---
+++ COMP3402_T7_ANT/PostFix/PostFix.java
@@ -0,0 +1,54 @@
+import java.util.Stack;
+
+public class PostFix {
+
+	public static void main(String[] args) {
+		String [] testcase = { "6", "1", "3", "4", "/", "-", "/" };
+		PostFix postfix = new PostFix(testcase);
+		System.out.println("Postfix " + postfix + " evaluates to "+postfix.evaluate());
+	}
+
+	private String[] tokens; 
+	public PostFix(String [] tokens) {
+		this.tokens = tokens;
+	}
+	
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
+		for(String s: tokens) {
+			sb.append(" "+s);
+		}
+		sb.deleteCharAt(0);
+		return sb.toString();
+	}
+	
+	public double evaluate() {
+		// Assume that the postfix is always valid
+		Stack<Double> stack = new Stack<Double>();
+		for(String token: tokens) {
+			if(token.equals("+")) {
+				stack.push(stack.pop()+stack.pop());	
+			} else if(token.equals("-")) {
+                // TODO
+				double tmp = stack.pop();
+				stack.push( stack.pop() - tmp );
+			} else if(token.equals("*")) {
+                // TODO
+				double tmp = stack.pop();
+				stack.push( stack.pop() * tmp );
+			} else if(token.equals("/")) {
+                // TODO
+				double tmp = stack.pop();
+				stack.push( stack.pop() / tmp );
+			} else if(token.equals("^")) {
+                // TODO
+				double tmp = stack.pop();
+				stack.push( Math.pow( stack.pop() , tmp ) );
+			} else {
+                // TODO
+				stack.push( Double.parseDouble(token) );
+			}
+		}
+		return stack.pop();
+	}
+}
